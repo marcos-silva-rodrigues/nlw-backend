d10f0698a96cb68c133760f2a4d505a987edce2b
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2023-01-19 21:41:10 -0300

    feat: rota para salvar um novo habito

---
+++ src/routes.ts
@@ -0,0 +1,30 @@
+import { FastifyInstance, FastifyRequest } from "fastify";
+import z from "zod";
+import dayjs from "dayjs";
+import { prisma } from "./lib/prisma";
+
+export async function appRoute(app: FastifyInstance) {
+  app.post("/habits", async (request: FastifyRequest) => {
+    const createHabitBody = z.object({
+      title: z.string(),
+      weekDays: z.array(z.number().min(0).max(6)),
+    });
+    const { title, weekDays } = createHabitBody.parse(request.body);
+
+		const today = dayjs().startOf('day').toDate()
+
+		await prisma.habit.create({
+			data: {
+				title,
+				created_at: today,
+				weekDays: {
+					create: weekDays.map(weekDay => {
+						return {
+							week_day: weekDay
+						}
+					})
+				}
+			}
+		})
+  });
+}
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,10 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors";
-import { PrismaClient } from "@prisma/client";
+import { appRoute } from "./routes";
 
 const app = Fastify();
-const prisma = new PrismaClient();
-
 app.register(cors);
-
-app.get("/habits", async () => {
-	const habits = await prisma.habit.findMany();
-	return habits;
-});
+app.register(appRoute);
 
 app.listen({
     port: 3333
